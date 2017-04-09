f3668038eb88a11326d2128e62dd019a3e122e53
The-on <the-on@The-ondeMacBook-Pro.local>
2017-04-09 19:01:26 +0800

    view/index

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,6 +3,6 @@ namespace app\controllers;
 use yii\web\Controller;
 class IndexController extends Controller{
 	public function actionIndex(){
-		echo "Index";
+		return $this->render('index');
 	}
 }
